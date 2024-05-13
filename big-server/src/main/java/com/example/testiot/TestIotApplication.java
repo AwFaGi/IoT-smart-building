214bdec9a7987c3f3924b10010b1129ba06cc6cf
Asgriim <andrew.vlasenkov70@gmail.com>
2024-05-13 16:07:14 +0300

    aboba

---
+++ big-server/src/main/java/com/example/testiot/TestIotApplication.java
@@ -0,0 +1,13 @@
+package com.example.testiot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestIotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestIotApplication.class, args);
+    }
+
+}
---
+++ big-server/src/test/java/com/example/testiot/TestIotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testiot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestIotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
